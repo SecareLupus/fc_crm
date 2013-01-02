a57afc80caf8959535ecc1c94bb138a524b5fbaa
Desmond Duval <haydre@gmail.com>
2013-01-01 23:00:45 -0500

    BUGFIX: After adding tasks, the link did not follow properly, fixed.

--- addtask.php
+++ addtask.php
@@ -10,8 +10,6 @@
    global $CUS_Category_Name;
    global $CUS_Assigned_Employee;
    global $CUS_Task_Summary;
-   global $CUS_Company_Website;
-   global $CUS_Company_CRMRoot;
    
    if($_POST['submit'] == 'submit')
    {
@@ -41,7 +39,7 @@
 			if ($row = mysqli_fetch_assoc($result))
 			{
 				extract($row);
-				echo "<a href='http://" . $CUS_Company_Website . $CUS_Company_CRMRoot . "showtask.php?TID=$TID'>Click Here to View Task</a><hr>";
+				echo "<a href='showtask.php?TID=$TID'>Click Here to View Task</a><hr>";
 			}
 			else
 			{
