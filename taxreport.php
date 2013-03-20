dbe003e5519477e4a29da57481ab867e6516f441
Desmond Duval <desmond@mrru.us>
2013-03-20 00:12:45 -0400

    General cleanup of taxreport.php

--- taxreport.php
+++ taxreport.php
@@ -6,11 +6,11 @@ include('datewidgets.inc');
 include('Report.inc');
 
 echo "<form name='buildReport' method='post'>";
-echo "<h3>Start Date</h3>";
-selDateWidget("startDate");
-echo "<h3>End Date</h3>";
-selDateWidget("endDate");
-echo "<br><input type='submit' name='submit' value='Build Report'><br>";
+echo "<h4>Start Date</h4>";
+selDateWidget("startDate", -1, -1, $_POST['startDate_Month'], $_POST['startDate_Day'], $_POST['startDate_Year']);
+echo "<h4>End Date</h4>";
+selDateWidget("endDate", -1, -1, $_POST['endDate_Month'], $_POST['endDate_Day'], $_POST['endDate_Year']);
+echo "<br><br><input type='submit' name='submit' value='Build Report'><br>";
 echo "</form>";
 
 if ($_POST['submit'] == "Build Report")
@@ -22,7 +22,7 @@ if ($_POST['submit'] == "Build Report")
 	$labour = $report->getCombinedLabourBilled();
 	$tax = $report->getCombinedTaxBilled();
 	
-	echo "Report Successfully Built.<br>";
+	echo "Report Successfully Built.<hr>";
 	$startDate = date("F j, Y", mktime(0, 0, 0, $_POST['startDate_Month'], $_POST['startDate_Day'], $_POST['startDate_Year']));
 	$endDate = date("F j, Y", mktime(0, 0, 0, $_POST['endDate_Month'], $_POST['endDate_Day'], $_POST['endDate_Year']));
 	echo "<br>Report for period between $startDate and $endDate<br><br>";
