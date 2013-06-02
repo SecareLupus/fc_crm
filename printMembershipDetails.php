c005eb88ee6831936450106b32a64da5632efa87
Desmond Duval <desmond@mrru.us>
2013-06-02 03:09:55 -0400

    Initial Implementation of Membership Plan Manager Still significant work to be done on it, but it's safe enough to merge and upstream.

---
+++ printMembershipDetails.php
@@ -0,0 +1,54 @@
+<?php
+
+   include('funcs.inc');
+   include_once('Warranty.inc');
+   include_once('Contact.inc');
+   
+	if($_GET['SID'])
+	{
+		$workingIMEI = $_GET['SID'];
+	}
+	else
+	{
+		die("No membership plan chosen.<hr>");
+	}
+
+   if($currSub = new WarrantySub($workingIMEI))
+   {
+   
+   echo "<table>
+   <tr>
+		<td rowspan=3 height=100 width=100><img width=100 src='./data/logo.png' ALT='$CUS_Company_Name Logo' /></td>
+		<td align='center'>$CUS_Company_Name</td></tr>
+   <tr>
+		<td align='center'>Member File</td></tr>
+   <tr><td>
+		<table>
+		<td width=100 align='left'>Member #" . $workingIMEI . "</td>
+		<td>Lead $CUS_Employee_Title: ";
+			printAgent($_SESSION['ID'], 1);
+echo 	"</td>
+		</table>
+   </td></tr>
+   </table>
+         <hr>";
+         
+		$currPlan = $currSub->getPlan();
+		echo "<h3>Plan Details</h3>";
+		echo "<table>";
+		echo "<tr><td width=250>Owner:</td>";
+		echo "<td width=500>". $currSub->getOwner()->getName() ."</td></tr>";
+		echo "<tr><td>IMEI/MEID:</td>";
+		echo "<td>". $currSub->getID() ."</td></tr>";
+		echo "<tr><td>Membership Dues:</td>";
+		echo "<td>". money($currPlan->getPremium()) ."</td></tr>";
+		echo "<tr><td>Service Fee:</td>";
+		echo "<td>". money($currPlan->getDeductible()) ."</td></tr>";
+		echo "<tr><td>Status:</td>";
+		$statusText = ($currSub->isActive() ? "Active" : "Inactive");
+		echo "<td>$statusText</td></tr>";		
+		echo "<tr><td>Due:</td>";
+		echo "<td>".$currSub->getDue()."</td></tr>";
+		echo "</table>";
+   }
+?>
---
+++ warranty.php
@@ -0,0 +1,92 @@
+<?php
+	include('funcs.inc');
+	include('member.inc');
+	include('header.php');
+	include_once('Contact.inc');
+	include_once('Warranty.inc');
+	
+	$workingIMEI = -1;
+	if($_POST['submit'] == 'Enroll')
+	{
+		$workingIMEI = $_POST['enrollIMEI'];
+		$enrollee = WarrantySub::createWarrantySub($workingIMEI);
+		$enrollee->setOwner(new Contact($_POST['enrollContact']), false);
+		if ($enrollee->setPlan($_POST['enrollPlan']))
+		{
+			$name = new Contact($_POST['enrollContact']);
+			echo $name->getName() . " has been successfully enrolled as a member!<hr>";
+		}
+	}
+	if($_GET['INQ'])
+	{
+		$workingIMEI = $_GET['INQ'];
+	}
+	if($_POST['ReUp'])
+	{
+		$workingIMEI = $_POST['hiddenIMEI'];
+		$workingSub = new WarrantySub($workingIMEI);
+		$workingSub->addTerms(1);
+	}
+	
+	
+	echo "<h2>Membership Plans</h2><hr>";
+	echo "<h3>Inquire</h3>";
+	echo "<form method='get'>";
+	WarrantySub::selectWarrantyOwner("INQ", $workingIMEI);
+	echo "<br><input type='submit' value='Lookup'>";
+	echo "</form>";
+	echo "<hr>";
+	
+	if ($workingIMEI == -1)
+	{
+		echo "<h3>Enroll</h3>";
+		echo "<form method='post'>";
+		echo "<table>";
+		echo "<tr><td width=250>Owner:</td>";
+		echo "<td width=500>";
+		selectCustomer("enrollContact", -1);
+		echo "</td></tr>";
+		echo "<tr><td>Plan:</td>";
+		echo "<td>";
+		WarrantyPlan::selectWarrantyPlan("enrollPlan", -1);
+		echo "</td></tr>";
+		echo "<tr><td>IMEI/MEID:</td>";
+		echo "<td><input type='text' name='enrollIMEI'></td></tr>";
+		echo "</table>";
+		echo "<input type='submit' name='submit' value='Enroll'>";
+		echo "</form>";
+	}
+	else
+	{
+		$currSub = new WarrantySub($workingIMEI);
+		$currPlan = $currSub->getPlan();
+		echo "<h3>Plan Details</h3>";
+		echo "<table>";
+		echo "<tr><td width=250>Owner:</td>";
+		echo "<td width=500>". $currSub->getOwner()->getName() ."</td></tr>";
+		echo "<tr><td>IMEI/MEID:</td>";
+		echo "<td>". $currSub->getID() ."</td></tr>";
+		echo "<tr><td>Membership Dues:</td>";
+		echo "<td>". money($currPlan->getPremium()) ."</td></tr>";
+		echo "<tr><td>Service Fee:</td>";
+		echo "<td>". money($currPlan->getDeductible()) ."</td></tr>";
+		echo "<tr><td>Status:</td>";
+		$statusText = ($currSub->isActive() ? "Active" : "Inactive");
+		echo "<td>$statusText</td></tr>";		
+		echo "<tr><td>Due:</td>";
+		echo "<td>".$currSub->getDue()."</td></tr>";
+		echo "</table>";
+		$buttonText = ($currSub->isActive() ? "Add Additional Term" : "Pay to Current");
+		echo "<form method='post'><input type='hidden' name='hiddenIMEI' value='$workingIMEI'>";
+		echo "<input type='submit' name='ReUp' value='$buttonText'></form>";
+		echo "<table><tr>";
+		echo "<td width=150><a href='printMembershipDetails.php?SID=$workingIMEI'>Print Summary</a></td>";
+		echo "<td width=150>Print Agreement</td>";
+		echo "</tr></table>";
+	}
+	echo "<hr>";
+	echo "<table><tr>";
+	echo "<td width=250>View Past-Due Members</td>";
+	echo "<td width=250>Edit Membership Plans</td>";
+	echo "</tr></table>";
+?>
